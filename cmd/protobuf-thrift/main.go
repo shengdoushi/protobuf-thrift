9c6d416019f93b7044e10b2d3fedd08aebebbdfb
guolihui <guolihui112@gmail.com>
2023-11-02 09:04:49 +0800

    add cmd/protobuf-thrift dir

---
+++ cmd/protobuf-thrift/main.go
@@ -0,0 +1,22 @@
+package protobuf_thrift
+
+import (
+	pbThrift "github.com/YYCoder/protobuf-thrift"
+	"github.com/YYCoder/protobuf-thrift/utils/logger"
+)
+
+func main() {
+	runner, err := pbThrift.NewRunner()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Info("Convert started, please wait.")
+
+	err = runner.Run()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Info("Convert succeeded.")
+}
